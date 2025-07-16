18029ccf6454a60881661230dfd263562bc96f0b
Christopher Mena <you@chrismena.dev>
2025-07-16 12:11:19 -0400

    Refactor http_server.h to define message types and queue structure for HTTP monitoring

--- weather-home-station-esp32/main/http_server.h
+++ weather-home-station-esp32/main/http_server.h
@@ -5,7 +5,48 @@
  *      Author: christophermena
  */
 
- #ifndef MAIN_HTTP_SERVER_H_
- #define MAIN_HTTP_SERVER_H_
+#ifndef MAIN_HTTP_SERVER_H_
+#define MAIN_HTTP_SERVER_H_
 
- #endif /* MAIN_HTTP_SERVER_H_ */
+#include "freertos/FreeRTOS.h"
+
+/**
+ * Messages for the HTTP monitor.
+ */
+typedef enum http_server_message
+{
+    HTTP_MSG_WIFI_CONNECT_INIT = 0,
+    HTTP_MSG_WIFI_CONNECT_SUCCESS,
+    HTTP_MSG_WIFI_CONNECT_FAIL,
+    HTTP_MSG_OTA_UPDATE_SUCCESSFUL,
+    HTTP_MSG_OTA_UPDATE_FAILED,
+    HTTP_MSG_OTA_UPDATE_INITIALIZED,
+} http_server_message_e;
+
+/**
+ * Structure for the message queue.
+*/
+typedef struct http_server_queue_message
+{
+    http_server_message_e msgID;
+} http_server_queue_message_t;
+
+/**
+ * Sends a message to the queue
+ * @param msgID message ID from the http_server_message_e enum.
+ * @return pdTRUE if an item was succesfully sent to the queue, otherwise pdFALSE.
+ * @note Expand the parameters liszt based on your requirements.
+ */
+BaseType_t http_server_monitor_send_message(http_server_message_e msgID);
+
+/**
+ * Starts the HTTP server.
+ */
+void http_server_start(void);
+
+/**
+ * Stops the HTTP server.
+ */
+void http_server_stop(void);
+
+#endif /* MAIN_HTTP_SERVER_H_ */
