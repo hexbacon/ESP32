d46f9959e0a3e6065ec4ea90c662efe5d915562a
Christopher Mena <you@chrismena.dev>
2025-07-16 13:12:47 -0400

    Add HTTP request handlers for JQuery, index.html, app.css, app.js, and favicon.ico

--- weather-home-station-esp32/main/http_server.c
+++ weather-home-station-esp32/main/http_server.c
@@ -31,6 +31,82 @@ extern const uint8_t favicon_ico_start[]            asm("_binary_favicon_ico_sta
 extern const uint8_t favicon_ico_end[]              asm("_binary_favicon_ico_end");
 
 /**
+ * JQuery get handler is requested when accessing the webpage.
+ * @param req HTTP request for which the uri needs to be handled.
+ * @return ESP_OK
+ */
+static esp_err_t http_server_jquery_handler(httpd_req_t *req)
+{
+    ESP_LOGI(TAG, "JQuery requested");
+    
+    httpd_resp_set_type(req, "application/javascript");
+    httpd_resp_send(req, (const char *) jquery_3_3_1_min_js_start, jquery_3_3_1_min_js_end - jquery_3_3_1_min_js_start);
+
+    return ESP_OK;
+}
+
+/**
+ * Sends the index.html page.
+ * @param req HTTP request for which the uri needs to be handled.
+ * @return ESP_OK
+ */
+static esp_err_t http_server_index_html_handler(httpd_req_t *req)
+{
+    ESP_LOGI(TAG, "index.html requested");
+    
+    httpd_resp_set_type(req, "text/html");
+    httpd_resp_send(req, (const char *) index_html_start, index_html_end - index_html_start);
+
+    return ESP_OK;
+}
+
+/**
+ * app.css get handler is requested when accessing the webpage.
+ * @param req HTTP request for which the uri needs to be handled.
+ * @return ESP_OK
+ */
+static esp_err_t http_server_app_css_handler(httpd_req_t *req)
+{
+    ESP_LOGI(TAG, "app.css requested");
+    
+    httpd_resp_set_type(req, "text/css");
+    httpd_resp_send(req, (const char *) app_css_start, app_css_end - app_css_start);
+
+    return ESP_OK;
+}
+
+/**
+ * app.js get handler is requested when accessing the webpage.
+ * @param req HTTP request for which the uri needs to be handled.
+ * @return ESP_OK
+ */
+static esp_err_t http_server_app_js_handler(httpd_req_t *req)
+{
+    ESP_LOGI(TAG, "app.js requested");
+    
+    httpd_resp_set_type(req, "application/javascript");
+    httpd_resp_send(req, (const char *) app_js_start, app_js_end - app_js_start);
+
+    return ESP_OK;
+}
+
+/**
+ * Sends the .ico (icon) file when accessing the web page.
+ * @param req HTTP request for which the uri needs to be handled.
+ * @return ESP_OK
+ */
+static esp_err_t http_server_favicon_ico_handler(httpd_req_t *req)
+{
+    ESP_LOGI(TAG, "favicon.ico requested");
+    
+    httpd_resp_set_type(req, "image/x-icon");
+    httpd_resp_send(req, (const char *) favicon_ico_start, favicon_ico_end - favicon_ico_start);
+
+    return ESP_OK;
+}
+
+
+/**
  * Sets up the default httpd server configuration
  * @return http server instance handle if successful, NULL otherwise.
  */
