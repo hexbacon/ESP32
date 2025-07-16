2c780cf5509fa54727c435cbe54275f6258e2a4d
Christopher Mena <you@chrismena.dev>
2025-07-16 11:49:18 -0400

    Add http_server.c and http_server.h files; update CMakeLists.txt to include http_server.c

---
+++ weather-home-station-esp32/main/http_server.h
@@ -0,0 +1,11 @@
+/*
+ * http_sever.h
+ *
+ *  Created on: Jul 16, 2025
+ *      Author: christophermena
+ */
+
+ #ifndef MAIN_HTTP_SERVER_H_
+ #define MAIN_HTTP_SERVER_H_
+
+ #endif /* MAIN_HTTP_SERVER_H_ */
